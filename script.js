602bc376fd042651a9b20874855cccaf82474a7a
Synobbol <97944378+Synobbol@users.noreply.github.com>
2023-01-31 23:03:40 +0100

    Ask number of squares

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+const main = document.querySelector("main");
 const container = document.querySelector("#container");
+const start = document.querySelector(".start");
+
+start.addEventListener("click", () =>{
+    let userChoice = Number(window.prompt("How many squares per side for the new grid?"));
+    
+    if(userChoice < 100 ){
+        createDiv(userChoice);
+    }
+    else{
+        container.textContent = "ERROR: number of squares must be under 100!";
+        container.style.color = "red";
+        return main.appendChild(container);
+    }
+});
 
 function createDiv(number = 16){
     container.setAttribute("style", `grid-template-columns: repeat(${number}, auto);`)
     // if number < 100 number = prompt();
     for (let index = 0; index < number * number; index++) {
         let divs = document.createElement("div");
-        //divs.classList.add("hoverColor");
         divs.setAttribute("style", "border: 1px solid pink;");
-        divs.addEventListener("mouseover", addHover );
-        divs.addEventListener("mouseout", removeHover );
+        divs.addEventListener("mouseover", addHover);
+        divs.addEventListener("mouseout", removeHover);
         
         container.appendChild(divs);
         
@@ -17,13 +31,11 @@ function createDiv(number = 16){
 
 function addHover(){
     console.log("addHover");
-    this.setAttribute("style", "transition: 0.7s; background-color: pink;");
+    this.setAttribute("style", "transition: 0.7s; background-color: pink; cursor:pointer;");
     
 }
 
 function removeHover(){
     console.log("removeHover");
-    this.setAttribute("style", "transition: 0.7s;");
+    this.setAttribute("style", "transition: 0.7s; cursor:pointer;");
 }
-
-createDiv();
