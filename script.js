1a4490da343754e3ae3fdafd2f5f539f2800486b
Synobbol <97944378+Synobbol@users.noreply.github.com>
2023-01-31 23:43:58 +0100

    Remove old grid and generate new grid

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const main = document.querySelector("main");
 const container = document.querySelector("#container");
+const main = document.querySelector("main");
 const start = document.querySelector(".start");
 
+
+
 start.addEventListener("click", () =>{
+   
     let userChoice = Number(window.prompt("How many squares per side for the new grid?"));
-    
+
     if(userChoice < 100 ){
+        removeDiv();
         createDiv(userChoice);
+        
     }
     else{
         container.textContent = "ERROR: number of squares must be under 100!";
@@ -17,25 +22,22 @@ start.addEventListener("click", () =>{
 
 function createDiv(number = 16){
     container.setAttribute("style", `grid-template-columns: repeat(${number}, auto);`)
-    // if number < 100 number = prompt();
+   
     for (let index = 0; index < number * number; index++) {
         let divs = document.createElement("div");
+
         divs.setAttribute("style", "border: 1px solid pink;");
         divs.addEventListener("mouseover", addHover);
-        divs.addEventListener("mouseout", removeHover);
         
         container.appendChild(divs);
-        
     }
 }
 
 function addHover(){
-    console.log("addHover");
     this.setAttribute("style", "transition: 0.7s; background-color: pink; cursor:pointer;");
     
 }
 
-function removeHover(){
-    console.log("removeHover");
-    this.setAttribute("style", "transition: 0.7s; cursor:pointer;");
+function removeDiv(){
+    container.textContent = "";
 }
